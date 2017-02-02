73daf512a3c02dcf23a75cbdb825d17bb0479fae
jagaa <jagaa1103@gmail.com>
2017-02-02 11:52:49 +0900

    init

---
+++ VideoPlayer/VideoPlayer/PlaylistViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PlaylistViewController.swift
+//  VideoPlayer
+//
+//  Created by Enkhjargal Gansukh on 02/02/2017.
+//  Copyright © 2017 Enkhjargal Gansukh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlaylistViewController: UIViewController, UITableViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+    }
+    
+    
+}
---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Enkhjargal Gansukh on 02/02/2017.
+//  Copyright © 2017 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
